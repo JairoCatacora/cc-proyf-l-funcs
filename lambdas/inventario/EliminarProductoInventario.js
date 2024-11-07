3fac3674aea345bd4f5df9cb52e1f205ef181a1e
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-07 05:08:03 +0000

    Create EliminarProductoInventario.js

---
+++ lambdas/inventario/EliminarProductoInventario.js
@@ -0,0 +1,22 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.lambda_handler = async (event) => {
+    const { tenant_id, product_id } = event.pathParameters;
+
+    const params = {
+        TableName: 't_inventarios',
+        Key: { tenant_id, product_id }
+    };
+
+    await dynamodb.delete(params).promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Producto eliminado del inventario',
+            tenant_id,
+            product_id
+        })
+    };
+};
