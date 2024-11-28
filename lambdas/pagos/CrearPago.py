9a6be25411d73abd8f68211d42381ef931e7a968
sofkp <sofia.ku@utec.edu.pe>
2024-11-27 19:42:52 -0500

    pagos a

--- lambdas/pagos/CrearPago.py
+++ lambdas/pagos/CrearPago.py
@@ -2,6 +2,7 @@ import boto3
 import json
 from datetime import datetime
 import urllib3
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 table_payments = dynamodb.Table('pf_pagos')
@@ -9,6 +10,10 @@ http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
     try:
+        # Verifica si 'body' está presente y es una cadena que necesita ser convertida a un diccionario
+        if isinstance(event['body'], str):
+            event['body'] = json.loads(event['body'])
+
         tenant_id = event['body']['tenant_id']
         pago_id = event['body']['pago_id']
         order_id = event['body']['order_id']
@@ -17,15 +22,17 @@ def lambda_handler(event, context):
 
         fecha_pago = datetime.utcnow().isoformat()
 
+        # Realiza la llamada GET al endpoint de la orden
         url = f"https://3j1d1u98t7.execute-api.us-east-1.amazonaws.com/dev/orden/search?user_id={user_id}&order_id={order_id}"
         response = http.request('GET', url)
         product_data = json.loads(response.data.decode('utf-8'))['body']
-        total = Decimal(str(product_data['totoal_price']))
+        total = Decimal(str(product_data['total_price']))  # Corregido el nombre del campo
 
+        # Inserta el pago en DynamoDB
         table_payments.put_item(
             Item={
                 'tenant_id': tenant_id,
-                'pago_id': payment_id,
+                'pago_id': pago_id,  # Corrección aquí: cambia 'payment_id' a 'pago_id'
                 'order_id': order_id,
                 'total': total,
                 'fecha_pago': fecha_pago,
@@ -33,24 +40,25 @@ def lambda_handler(event, context):
             }
         )
 
+        # Llamada POST para actualizar el estado de la orden
         url = f"https://3j1d1u98t7.execute-api.us-east-1.amazonaws.com/dev/orden/update"
         body = {
-            "tenant_id" : tenant_id,
-            "order_id" : order_id,
-            "order_status" : 'APPROVED PAYMENT'
+            "tenant_id": tenant_id,
+            "order_id": order_id,
+            "order_status": 'APPROVED PAYMENT'
         }
         encoded_body = json.dumps(body)
 
         response = http.request(
             "POST",
             url,
-            body=encoded_body
+            body=encoded_body,
+            headers={'Content-Type': 'application/json'}
         )
-
 
         return {
             'statusCode': 201,
-            'body': {'message': 'Payment created successfully', 'payment_id': payment_id}
+            'body': {'message': 'Payment created successfully', 'payment_id': pago_id}
         }
     except Exception as e:
         return {
