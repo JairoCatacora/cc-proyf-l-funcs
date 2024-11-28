72a368fba7f4d68bd72fc02186c05cae17b49bb8
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-27 22:48:04 -0500

    Update and rename GetTenantLogos.js to getTenantLogos.js

--- lambdas/personalizacion/getTenantLogos.js
+++ lambdas/personalizacion/getTenantLogos.js
@@ -1,18 +1,18 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const BUCKET_NAME = 'dev-multishop-logos';
+const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3");
+const BUCKET_NAME = "dev-multishop-logos";
+const s3 = new S3Client({ region: "us-east-1" });
 
-exports.handler = async (event) => {
+exports.handler = async () => {
     try {
         const params = {
             Bucket: BUCKET_NAME,
         };
 
-        const response = await s3.listObjectsV2(params).promise();
-        const tenantLogos = response.Contents
-            .filter(item => item.Key.endsWith('.svg')) // Filtra solo archivos .svg
-            .map(item => ({
-                tenant_id: item.Key.replace('.svg', ''),
+        const response = await s3.send(new ListObjectsV2Command(params));
+        const tenantLogos = (response.Contents || [])
+            .filter((item) => item.Key.endsWith(".svg")) // Filtra solo archivos .svg
+            .map((item) => ({
+                tenant_id: item.Key.replace(".svg", ""),
                 logo_url: `https://${BUCKET_NAME}.s3.us-east-1.amazonaws.com/${item.Key}`
             }));
 
